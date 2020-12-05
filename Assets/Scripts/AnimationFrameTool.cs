415cbd824f6998df7d792af0713d144ca1e9d54a
rStar <starlove124760@gmail.com>
2020-12-06 00:16:41 +0800

    feat : add editor window

--- Assets/Scripts/AnimationFrameTool.cs
+++ Assets/Scripts/AnimationFrameTool.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 
-public class AnimationFrameTool : MonoBehaviour
+public class AnimationFrameTool : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    string myString = "Hello World";
+    bool   groupEnabled;
+    bool   myBool  = true;
+    float  myFloat = 1.23f;
+
+    public static AnimationFrameTool instance;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/AnimationFrameTool _F1")]
+    static void Init()
     {
-        
+        if (instance == null)
+        {
+            // Get existing open window or if none, make a new one:
+            AnimationFrameTool window = (AnimationFrameTool)EditorWindow.GetWindow(typeof(AnimationFrameTool));
+            window.Show();
+            instance = window;
+        }
+        else instance.Focus();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        var activeGameObject = Selection.activeGameObject;
+        if (activeGameObject != null)
+        {
+            GUILayout.Label($"Current Select {activeGameObject.name}" , EditorStyles.boldLabel);
+            var animator = activeGameObject.GetComponent<Animator>();
+            if (animator != null)
+            {
+                myString = EditorGUILayout.TextField("Text Field" , myString);
+
+                groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings" , groupEnabled);
+                myBool       = EditorGUILayout.Toggle("Toggle" , myBool);
+                myFloat      = EditorGUILayout.Slider("Slider" , myFloat , -3 , 3);
+                EditorGUILayout.EndToggleGroup();
+            }
+        }
     }
 }
