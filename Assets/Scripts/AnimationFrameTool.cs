cfa35272598ca73c6cadc2ada7fed53fb45aca6b
rStar <starlove124760@gmail.com>
2020-12-06 01:51:11 +0800

    feat : calculate correct frame number

--- Assets/Scripts/AnimationFrameTool.cs
+++ Assets/Scripts/AnimationFrameTool.cs
@@ -3,13 +3,44 @@ using UnityEditor;
 
 public class AnimationFrameTool : EditorWindow
 {
-    string myString = "Hello World";
-    bool   groupEnabled;
-    bool   myBool  = true;
-    float  myFloat = 1.23f;
-
     public static  AnimationFrameTool instance;
     private static float              _perFrameSecond = 1f / 30f;
+    bool                              groupEnabled;
+    bool                              myBool       = true;
+    float                             myFloat      = 1.23f;
+    string                            myString     = "Hello World";
+    private int                       currentFrame = 0;
+
+    void OnGUI()
+    {
+        var activeGameObject = Selection.activeGameObject;
+
+        var isPlaying = Application.isPlaying;
+        if (activeGameObject != null)
+        {
+            GUILayout.Label($"Current Select {activeGameObject.name}" , EditorStyles.boldLabel);
+            var animator = activeGameObject.GetComponent<Animator>();
+            if (animator != null && isPlaying)
+            {
+                var   clipInfo                 = animator.GetCurrentAnimatorClipInfo(0)[0];
+                var   currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+                var   clip                     = clipInfo.clip;
+                var   clipWeight               = clipInfo.weight;
+                var   clipFrameRate            = clip.frameRate;
+                var   clipLength               = clip.length;
+                var   frameTime                = 1 / clipFrameRate * currentFrame;
+                var   normalizedTime           = currentAnimatorStateInfo.normalizedTime;
+                float time                     = clipLength * normalizedTime;
+                if (time >= frameTime) currentFrame++;
+                Debug.Log($"{time} , {normalizedTime} , {currentFrame - 1}");
+            }
+        }
+
+        if (isPlaying == false)
+        {
+            currentFrame = 0;
+        }
+    }
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("Window/AnimationFrameTool _F1")]
@@ -23,30 +54,5 @@ public class AnimationFrameTool : EditorWindow
             instance = window;
         }
         else instance.Focus();
-    }
-
-    void OnGUI()
-    {
-        var activeGameObject = Selection.activeGameObject;
-
-        if (activeGameObject != null)
-        {
-            GUILayout.Label($"Current Select {activeGameObject.name}" , EditorStyles.boldLabel);
-            var animator  = activeGameObject.GetComponent<Animator>();
-            var isPlaying = Application.isPlaying;
-            if (animator != null && isPlaying)
-            {
-                var clipInfo                 = animator.GetCurrentAnimatorClipInfo(0)[0];
-                var currentAnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
-                var clipWeight               = clipInfo.weight;
-                var clip                     = clipInfo.clip;
-                var clipFrameRate            = clip.frameRate;
-                var clipLength               = clip.length;
-                var length                   = currentAnimatorStateInfo.length;
-                var normalizedTime           = currentAnimatorStateInfo.normalizedTime;
-                var currentFrame             = /*(int)*/(clipWeight * (normalizedTime * 30));
-                Debug.Log($"{clipFrameRate} , {clipWeight} , {normalizedTime} , {currentFrame}");
-            }
-        }
     }
 }
